3e4b4696ac8c93b48c34811e35e919893a9c731c
ramasrikanth <143990651+22K61A4237@users.noreply.github.com>
2023-09-26 22:20:01 -0700

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,8 @@
+public class Myclass
+{
+	public static void main(String[] args) {
+		System.out.println("my name is ramya");
+System.out.println("cse-aiml");
+System.out.println("22K61A4238");
+	}
+}
